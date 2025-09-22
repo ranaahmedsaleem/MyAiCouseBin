8afabe13b66773c1676d63953f449ca2e94dbbab
ranaahmedsaleem <ahmedsal33m25@gmail.com>
2025-09-22 17:23:40 +0500

    Create practice1 test.py

---
+++ practice1 test.py
@@ -0,0 +1,11 @@
+
+studentage=int(19)
+studentheight=float(5.8)
+studentname= "ahmed saleem"
+print("student age=",studentage)
+print("student height=",studentheight)
+print("student name =",studentname)
+
+print("student age=",type(studentage))
+print("student height=",type(studentheight))
+print("student name =",type(studentname))
