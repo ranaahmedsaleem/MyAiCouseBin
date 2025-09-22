7704a6307bff21fbd8f59d47a67d043697c97c44
ranaahmedsaleem <ahmedsal33m25@gmail.com>
2025-09-22 17:44:42 +0500

    Update practice 2 test.py
    sum and subtract done

--- practice 2 test.py
+++ practice 2 test.py
@@ -2,4 +2,8 @@ studentage=int(19)
 studentheight=float(5.8)
 sum = studentage+studentheight
 print("sum of age and height =",sum)
-print("sum of age and height =",type(sum))
+print("type of sum of age and height =",type(sum))
+
+subtract=studentage-studentheight
+print("subtract of age and height =",subtract)
+print("type of subtract of age and height =",type(subtract))
